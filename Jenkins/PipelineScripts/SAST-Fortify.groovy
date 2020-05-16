cb0b3e120bb579309198d378675705f74d8b9953
Maxpowersi <maxi1990@gmail.com>
2020-05-16 18:35:07 -0300

    Update SAST-Fortify.groovy

--- Jenkins/PipelineScripts/SAST-Fortify.groovy
+++ Jenkins/PipelineScripts/SAST-Fortify.groovy
@@ -7,14 +7,13 @@ vuls = [:]
 def runStage(bsiToken, sourCodePath){
 	fodStaticAssessment bsiToken: bsiToken, 
 	entitlementPreference: 'SingleScanOnly', 
-	inProgressScanActionType: 'CancelInProgressScan', 
-	personalAccessToken: 'JiraOauthAccess', 
+	inProgressScanActionType: 'CancelInProgressScan',  
 	remediationScanPreferenceType: 'NonRemediationScanOnly', 
 	srcLocation: sourCodePath
 }
 
 def getResults(bsiToken){
-	def response = fodPollResults bsiToken: bsiToken, personalAccessToken: 'FoDApiKey', pollingInterval: 10
+	def response = fodPollResults bsiToken: bsiToken, pollingInterval: 10
 }
 
 def getFortifyResult(){
