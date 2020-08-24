904c91f76a5e5599bf328641695903a35a862107
leomarazzo <leonardojmarazzo@gmail.com>
2020-08-24 11:10:40 -0300

    Correcciones

--- Jenkins/PipelineScripts/Install-dotNetDependecies.groovy
+++ Jenkins/PipelineScripts/Install-dotNetDependecies.groovy
@@ -3,7 +3,7 @@ def runStage(){
 	slackSend color: 'good', message: 'Installing dependencies...'
 
 	try {
-		sh """find . -name \\"*.sln\\" -exec dotnet restore \\'{}\\' \\\\\\;"""
+		sh 'find . -name *.sln -exec dotnet restore {} ";"'
 
 		slackSend color: 'good', message: 'dotnet restore: SUCCESS'
 		slackSend channel: 'notificaciones_cliente', color: 'good', message: 'Dependency installation was successful. Starting SAST...'
