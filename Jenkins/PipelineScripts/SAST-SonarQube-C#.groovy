06e46795fd54854e81dc42ea3c4445fc0bf6df60
leomarazzo <leonardojmarazzo@gmail.com>
2020-08-24 10:46:16 -0300

    Correcciones

--- Jenkins/PipelineScripts/SAST-SonarQube-C#.groovy
+++ Jenkins/PipelineScripts/SAST-SonarQube-C#.groovy
@@ -2,7 +2,7 @@ def runStage(){
 
     try {
         def projname = env.JOB_NAME
-        sh "/root/.dotnet/tools/dotnet-sonarscanner begin /k:${projname} /d:sonar.login=${env.sonartoken} /d:sonar.host.url=${env.SASTIP}:${env.sonarport}"
+        sh "/root/.dotnet/tools/dotnet-sonarscanner begin /k:${projname} /d:sonar.login=${env.sonartoken} /d:sonar.host.url=http://${env.SASTIP}:${env.sonarport}"
         sh """find . -name \\"*.sln\\" -exec dotnet build {} \\\\\\;"""
         sh "/root/.dotnet/tools/dotnet-sonarscanner end"
          
