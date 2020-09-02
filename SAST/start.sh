5b6678cd68d4244577519668c9a5218cfebcf5a4
Maxpowersi <maxi1990@gmail.com>
2020-09-02 17:25:57 -0300

    Update start.sh

--- SAST/start.sh
+++ SAST/start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#Los pasos a continuacion permiten crear la imagen de sonar correctamente creando tambien los perfiles.
+
+#1) Configurar en el script configure.sh las variables githubusr y githubpasswd con usuario y contraseña, respectivamente, de una cuenta de github con acceso al repositorio [SonarSecurityRules](https://github.com/badBounty/SonarSecurityRules)
+
+#2) Ejecutar el archivo start.sh de la siguiente manera para buildear y correr la imagen de sonar
+
+#```
+#start.sh {build | nobuild} {container name} {running sonar port} {ssh port}
+#```
 
 #Los pasos a continuacion permiten crear la imagen de sonar correctamente creando tambien los perfiles.
 
