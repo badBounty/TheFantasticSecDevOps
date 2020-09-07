b13b24069e9e99e18236083c00ddecd247af6217
leomarazzo <leonardojmarazzo@gmail.com>
2020-09-07 12:01:51 -0300

    Correcciones

--- Jenkins/PipelineScripts/SAST-Deployment.groovy
+++ Jenkins/PipelineScripts/SAST-Deployment.groovy
@@ -2,8 +2,7 @@ def runStage(){
     try {
         sshagent(['ssh-key-vm']) {
             def projname = env.JOB_NAME
-            //sh "ssh -o StrictHostKeyChecking=no ${env.SASTVMUSER}@${env.SASTIP} screen -d -m ${env.repositoryFolder}/start.sh nobuild ${projname} ${env.sonarport} ${env.port}"
-            sh "ssh -o StrictHostKeyChecking=no ${env.SASTVMUSER}@${env.SASTIP} ${env.repositoryFolder}/start.sh nobuild ${projname} ${env.sonarport} ${env.port}"
+            sh "ssh -o StrictHostKeyChecking=no ${env.SASTVMUSER}@${env.SASTIP} screen -d -m ${env.repositoryFolder}/start.sh nobuild ${projname} ${env.sonarport} ${env.port}"
             sh 'sleep 5m'
         }
         print('------Stage "SAST Deploymeny": Success ------')
