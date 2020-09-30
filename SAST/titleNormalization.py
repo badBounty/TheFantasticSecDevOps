5b3261f7bc65572b544d0df0fef21d470a4e0f78
leomarazzo <leonardojmarazzo@gmail.com>
2020-09-30 11:53:47 -0300

    Add logger

--- SAST/titleNormalization.py
+++ SAST/titleNormalization.py
@@ -1,7 +1,24 @@
 import re
 import json
 import sys
+import logging
+import sys
 
+
+def setup_logger(name, log_file, level=logging.DEBUG, console=True):
+    """To setup as many loggers as you want"""
+    
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    handler = logging.FileHandler(log_file)        
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+    if console:
+        logger.addHandler(logging.StreamHandler(sys.stdout))
+
+    return logger
 
 def normalizeTitle(title):
     j = json.load(open("/home/normalization.json", 'r'))
@@ -9,10 +26,12 @@ def normalizeTitle(title):
         normalizedTitle = j[title]
         print(normalizedTitle)
     except:
+
         print("")
 
 
 if __name__ == "__main__":
+    logger = setup_logger("vulnsNormalization", "titleNormalization.log", console=False)
     if (len(sys.argv) > 2):
         args = sys.argv[1:]
         arg = " ".join(args)
