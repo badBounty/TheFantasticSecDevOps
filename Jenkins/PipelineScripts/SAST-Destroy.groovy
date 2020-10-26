d69f475da6bf0049611359c67fc5ced7a7b5851b
leomarazzo <leonardojmarazzo@gmail.com>
2020-10-26 14:49:55 -0300

    Correcciones

--- Jenkins/PipelineScripts/SAST-Destroy.groovy
+++ Jenkins/PipelineScripts/SAST-Destroy.groovy
@@ -5,16 +5,16 @@ def runStage()
         sshagent(['ssh-key-vm']) 
         {
             def projname = env.JOB_NAME
-            sh "ssh -o StrictHostKeyChecking=no ${env.SASTVMUSER}@${env.SASTIP} docker container rm ${projname}"
+            sh "ssh -o StrictHostKeyChecking=no ${env.SASTVMUSER}@${env.SASTIP} docker container rm -f ${projname}"
         }
     }
     catch(Exception e) 
     {
         //TODO use notifier module
-		slackSend color: 'danger', message: 'Stage: "SAST-Destroy": FAILURE'
+		slackSend color: 'danger', message: 'Stage: "SAST-Deployment": FAILURE'
         
 		currentBuild.result = 'FAILURE'
-		print('Stage: "SAST-Destroy": FAILURE')
+		print('Stage: "SAST-Deployment": FAILURE')
         print(e.printStackTrace())
     }
 }
