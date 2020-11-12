4ba0557f8436e7df1b48c86f1ed6a20352004aa5
leomarazzo <leonardojmarazzo@gmail.com>
2020-11-12 16:13:26 -0300

    Update Scripts

--- Jenkins/PipelineScripts/SAST-Deployment.groovy
+++ Jenkins/PipelineScripts/SAST-Deployment.groovy
@@ -8,10 +8,11 @@ def runStage(notifier)
         {
             def projname = env.JOB_NAME
             sh "ssh-keygen -f '/var/jenkins_home/.ssh/known_hosts' -R [${env.SAST_Server_IP}]"
-            //screen -d -m
-            sh "ssh -o StrictHostKeyChecking=no ${env.SAST_Server_User}@${env.SAST_Server_IP} ${env.SAST_Server_Repository_SAST_Path}/start.sh nobuild ${projname} ${env.Sonar_Port} ${env.SAST_Server_SSH_Port}"
-            //sh 'sleep 15m'
+            //
+            sh "ssh -o StrictHostKeyChecking=no ${env.SAST_Server_User}@${env.SAST_Server_IP} screen -d -m ${env.SAST_Server_Repository_SAST_Path}/start.sh nobuild ${projname} ${env.Sonar_Port} ${env.SAST_Server_SSH_Port}"
+            sh 'sleep 15m'
             sh "ssh -o StrictHostKeyChecking=no ${env.SAST_Server_User}@${env.SAST_Server_IP} docker container ls -a"
+            sh "ssh -o StrictHostKeyChecking=no ${env.SAST_Server_User}@${env.SAST_Server_IP} ls -a -l"
         }
 
         notifier.sendMessage('','good','Stage: "SAST-Deployment": SUCCESS')
