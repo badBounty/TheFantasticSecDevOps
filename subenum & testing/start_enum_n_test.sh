aab0e6e6e28a6f6d571fd09638b5a52a4b5bfcf9
cangaruu <50958708+cangaruu@users.noreply.github.com>
2021-08-05 13:24:11 -0300

    Add files via upload

---
+++ subenum & testing/start_enum_n_test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# ./recon_main.sh [domain file list]
+
+# Enumeracion y descubrimiento de subdominios en base a [domain file list].
+
+DOMAINS=$(cat $1)
+
+for domain in $DOMAINS; do
+        firstc=${1:0:1}
+        if [ "$firstc" == "*" ]; then
+                domain_no_wc=''
+                echo $domain | sed 's/^..//' > $domain_no_wc
+                ./subdomain_enum.sh $domain_no_wc >> $1
+        else
+                echo "No wildcard for: " $domain
+		#echo $domain >> final_hosts.txt
+done
+
+# ------------------------- Nuclei testing START ------------------------------
+
+# while true; do
+echo "Launching Nuclei"
+mkdir nuclei_tests
+cd nuclei_tests
+echo "Updating Templates"
+nuclei -update-templates
+echo "Testing"
+
+for scan in $(cat ../nuclei-scans); do
+        echo "Launching template: " $scan
+        nuclei -l ../final_hosts.txt -t $scan/ -o $1-$scan.nuclei
+        cat $1-$scan.nuclei | slackcat --channel general --stream
+done
+echo "Results sent"
+# done
+
+# ------------------------- Nuclei testing END --------------------------------
---
+++ subenum & testing/subdomain_enum.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# amass => $1-amass_hosts.txt
+# massdns => $1-massDNS-hosts.txt
+# altdns => $-altDNS_hosts.txt
+
+# ----------------- Check if program exists ------------------------------
+
+checkprogram() {
+        if ! command -v "$1" &> /dev/null
+        then
+                echo "Error: $1 program could not be found"
+                exit
+        fi
+}
+
+checkprogram "nuclei"
+checkprogram "amass"
+checkprogram "massdns"
+checkprogram "altdns"
+# checkprogram "chrome" tengo la direccion del binario en $CHROME
+checkprogram "httprobe"
+checkprogram "aquatone"
+
+# ------------------- Program check END ----------------------------------
+
+# ------------------- Domain enumeration START ---------------------------
+
+RESULT=$1-amass_hosts.txt
+CHROME='~/Downloads/chrome-linux/chrome'
+
+echo "Subdomain discovery: Amass"
+amass enum -passive -d $1 -o $RESULT
+echo "Amass discovery: DONE"
+
+MASS_HOSTS=$1-massDNS_hosts.txt
+echo "DNS Resolve: massDNS"
+massdns -r resolvers.txt -q -t A -o S -w result.out $RESULT
+cat result.out | awk '{print $1}' | sed 's/.$//' > $MASS_HOSTS
+rm result.out
+echo "DNS resolution: DONE"
+
+echo "DNS Permutation and resolve: altDNS"
+ALT_HOSTS=$1-altDNS_hosts.txt
+altdns -i $MASS_HOSTS -o permuted_list.txt -w words.txt -r -s result.out -t 10
+cat result.out | awk '{print $1}' | sed 's/.$//' > $ALT_HOSTS
+rm result.out
+
+echo "Subdomains merge: massDNS + altDNS"
+cat $MASS_HOSTS >> hosts_merge.txt
+cat $ALT_HOSTS >> hosts_merge.txt
+
+# OUTPUT
+cat hosts_merge.txt | sort | uniq
+rm hosts_merge.txt
+rm $MASS_HOSTS
+rm $ALT_HOSTS
+rm $RESULT
+
+# ------------------------- Domain enumeration END ------------------------------
