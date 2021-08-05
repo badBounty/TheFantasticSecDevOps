91a09f23d82e9d6dd300ed47b6e79f1577b3606a
Maxpowersi <maxi1990@gmail.com>
2021-08-05 19:01:45 -0300

    Update configure.sh

--- SAST/configure.sh
+++ SAST/configure.sh
@@ -2,13 +2,21 @@
 
 wget --directory-prefix=/opt/sonarqube/extensions/plugins/ https://github.com/dependency-check/dependency-check-sonar-plugin/releases/download/2.0.4/sonar-dependency-check-plugin-2.0.4.jar
 
-cd /tmp
+#githubusr=leomarazzo #githubpasswd=Juli.0804
 
-git clone https://github.com/spotbugs/sonar-findbugs.git
+#cd /tmp
 
-cd /tmp/sonar-findbugs
-mvn clean install -DskipTests
-cp target/sonar-findbugs-plugin.jar /opt/sonarqube/extensions/plugins/
+#git clone https://$githubusr:$githubpasswd@github.com/badBounty/SonarSecurityRules.git
+#git clone https://github.com/spotbugs/sonar-findbugs.git
+
+#cd /tmp/sonar-findbugs
+#mvn clean install -DskipTests
+#cp target/sonar-findbugs-plugin.jar /opt/sonarqube/extensions/plugins/
+
+#cd /tmp/SonarSecurityRules
+#mvn clean install -DskipTests
+#cp target/*.jar /opt/sonarqube/extensions/plugins/
+
 
 cd /opt/sonarqube
 
