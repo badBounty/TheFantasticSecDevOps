56705651b000bb24fbfd37d7b1cfc959cbd600a0
cangaruu <50958708+cangaruu@users.noreply.github.com>
2021-08-05 16:47:12 -0300

    Update subdomain_enum.sh

--- DAST/Scripts/subdomain & test/subdomain_enum.sh
+++ DAST/Scripts/subdomain & test/subdomain_enum.sh
@@ -18,7 +18,24 @@ checkprogram() {
         if ! command -v "$1" &> /dev/null
         then
                 echo "Error: $1 program could not be found"
-                exit
+                echo "Installing: $1"
+                if [ "$1" == "nuclei" ]; then
+                        GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+                elif [ "$1" == "amass" ]; then
+                        brew tap caffix/amass
+                        brew install amass
+                elif [ "$1" == "aquatone" ]; then
+                        echo "[x] Could not install aquatone, please check: 'https://github.com/michenriksen/aquatone' for more instructions."
+                        exit
+                elif [ "$1" == "slackcat" ]; then
+                        curl -Lo slackcat https://github.com/bcicen/slackcat/releases/download/1.7.2/slackcat-1.7.2-$(uname -s)-amd64
+                        mv slackcat /usr/local/bin/
+                        chmod +x /usr/local/bin/slackcat
+                        echo "[+] Must configure slackcat to send alerts: execute 'slackcat --configure'"
+                        exit
+                else
+                        apt install $1
+                fi
         fi
 }
 
@@ -29,6 +46,13 @@ checkprogram "altdns"
 # checkprogram "chrome" tengo la direccion del binario en $CHROME
 checkprogram "httprobe"
 checkprogram "aquatone"
+checkprogram "slackcat"
+
+if [[ ! -f "resolvers.txt" ]]; then
+        wget https://github.com/blechschmidt/massdns/blob/master/lists/resolvers.txt
+elif [[ ! -f "words.txt" ]]; then
+        wget https://github.com/infosec-au/altdns/blob/master/words.txt
+fi
 
 # ------------------- Program check END ----------------------------------
 
