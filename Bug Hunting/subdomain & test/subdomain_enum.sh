31b1194bd84d5e27ae0ec7a612d8eed4a6dd07fc
Maxpowersi <maxi1990@gmail.com>
2021-08-20 13:52:20 -0300

    sublister

--- Bug Hunting/subdomain & test/subdomain_enum.sh
+++ Bug Hunting/subdomain & test/subdomain_enum.sh
@@ -57,16 +57,23 @@ fi
 
 # ------------------- Domain enumeration START ---------------------------
 
-RESULT=$1-amass_hosts.txt
+RESULT_AMASS=$1-amass_hosts.txt
 CHROME='~/Downloads/chrome-linux/chrome'
 
 echo "Subdomain discovery: Amass"
-amass enum -passive -d $1 -o $RESULT
-echo "Amass discovery: DONE"
+amass enum -passive -d $1 -o $RESULT_AMASS
+echo "Subdomain discovery: Amass: DONE"
 
+RESULT_SUBLISTER=$1-sublister_hosts.txt
 echo "Subdomain discovery: Sublister"
-#sublister
-echo "Amass discovery: DONE"#
+sublister -b -d $1  -o $RESULT_SUBLISTER
+echo "Subdomain discovery: Sublister DONE"#
+
+RESULT=$1-subdomains_hosts.txt
+echo "Merge amass and sublister results: START"
+cat  $RESULT_SUBLISTER $RESULT_AMASS > $RESULT
+sort $RESULT | uniq -u > $RESULT
+echo "Merge amass and sublister results: DONE"
 
 MASS_HOSTS=$1-massDNS_hosts.txt
 echo "DNS Resolve: massDNS"
@@ -81,8 +88,6 @@ altdns -i $MASS_HOSTS -o permuted_list.txt -w words.txt -r -s result.out -t 10
 cat result.out | awk '{print $1}' | sed 's/.$//' > $ALT_HOSTS
 rm result.out
 
-
-
 echo "Subdomains merge: massDNS + altDNS + sublister"
 cat $MASS_HOSTS >> hosts_merge.txt
 cat $ALT_HOSTS >> hosts_merge.txt
@@ -93,6 +98,7 @@ rm hosts_merge.txt
 rm $MASS_HOSTS
 rm $ALT_HOSTS
 rm $RESULT
+rm $RESULT_SUBLISTER
 
 # ------------------------- Domain enumeration END ------------------------------
 
