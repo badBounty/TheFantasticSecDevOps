73188710c86c36acf73b6fd639a15876037cff69
jftierno <jftierno@gmail.com>
2021-08-24 13:20:05 -0400

    Nuclei sending only new found.

--- Bug Hunting/nuclei_scan.sh
+++ Bug Hunting/nuclei_scan.sh
@@ -6,13 +6,41 @@ echo "nuclei scan - starting" | slackcat -c bug-hunter -s
 
 mkdir $1-nuclei_tests
 cd $1-nuclei_tests
-echo "nuclei scan - updating templates"  | slackcat -c bug-hunter -s
+echo "nuclei scan - updating templates" | slackcat -c bug-hunter -s
 nuclei -update-templates
 
-echo "nuclei scan - scanning"  | slackcat -c bug-hunter -s
+INITIALSEND="0"
+
+FINALO=$1-final.txt
+OFINALO=$1-oldfinal.txt
+
+if [ ! -f $FINALO ] 
+then
+	INITIALSEND="1"
+fi
+
+echo "nuclei scan - scanning" | slackcat -c bug-hunter -s
 for scan in $(cat ../nuclei-scans); do
-        nuclei -l $1 -t $scan/ -o $1-$scan.nuclei
-        cat $1-$scan.nuclei | slackcat -c bug-hunter -s
+        nuclei -l $1 -t $scan -o "$1-$scan.nuclei" -silent
+        cat $1-$scan.nuclei >> $FINALO
 done
 
-echo "nuclei scan - done"  | slackcat -c general -s
+if [ "${INITIALSEND}" -eq "1" ] 
+then
+	cat $FINALO | sort | uniq > $FINALO
+	slackcat -c bug-hunter $FINALO
+else
+	cat $FINALO | sort | uniq > $FINALO
+	if cmp --silent -- "$FINALO" "$OFINALO"; then
+        	echo "nuclei scan - nothing new found." | slackcat -c bug-hunter -s
+      	else
+        	NEWFOUND=newfound.txt
+        	comm -23 <(sort $FINALO) <(sort $OFINALO) > $NEWFOUND
+        	slackcat -c bug-hunter $NEWFOUND
+        	rm $NEWFOUND
+      fi
+fi
+
+cp $FINALO $OFINALO
+
+echo "nuclei scan - done"  | slackcat -c bug-hunter -s
