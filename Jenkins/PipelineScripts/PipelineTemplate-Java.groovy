703d34ddac8c5cbc811d048a48b3f7866bea6a6f
Kalust <39742600+Kalust@users.noreply.github.com>
2021-09-08 14:00:31 -0300

    Update PipelineTemplate-Java.groovy

--- Jenkins/PipelineScripts/PipelineTemplate-Java.groovy
+++ Jenkins/PipelineScripts/PipelineTemplate-Java.groovy
@@ -71,7 +71,6 @@ pipeline {
                         modules.SAST_SonarQube_Maven = load "Jenkins/PipelineScripts/SAST-SonarQube-Maven.groovy"
                         modules.SAST_SonarResults = load "Jenkins/PipelineScripts/SAST-SonarResults.groovy"
                         modules.SAST_Dependencies = load "Jenkins/PipelineScripts/SAST-Java-DependenciesCheck.groovy"
-                        //modules.SAST_Regex = load "Jenkins/PipelineScripts/SAST-RegexScanner.groovy"
                         modules.SAST_Destroy = load "Jenkins/PipelineScripts/SAST-Destroy.groovy"
                         modules.SAST_PostResults = load "Jenkins/PipelineScripts/SAST-PostResults.groovy"
                         modules.SAST_SendVulnsLog = load "Jenkins/PipelineScripts/SAST-SendVulnsLog.groovy"
@@ -169,19 +168,9 @@ pipeline {
                 }
             }
         }
-        /*
-        stage('SAST-RegexScanner'){
-            steps{
-                script{
-                    if (SkipBuild == 'YES'){
-                        currentBuild.result = 'SUCCESS'
-                        return
-                    }
-                    modules.SAST_Regex.runStage(modules.Notifier, vulns)
-                }
-            }
-        }
-        */
+        
+        //Nuclei scanner
+
         stage('SAST-Destroy'){
             steps{
                 script{
