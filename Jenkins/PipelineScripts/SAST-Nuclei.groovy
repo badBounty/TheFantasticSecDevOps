7965f3e80615413ebf852c0d70ff99d36f96e2ae
Kalust <39742600+Kalust@users.noreply.github.com>
2021-09-09 09:49:22 -0300

    Update SAST-Nuclei.groovy

--- Jenkins/PipelineScripts/SAST-Nuclei.groovy
+++ Jenkins/PipelineScripts/SAST-Nuclei.groovy
@@ -20,6 +20,7 @@ def runStage(notifier)
 	    sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} cp -rf /home/TheFantasticDevSecOps/SAST/Nuclei-Custom-Templates/* /root/nuclei-templates/file"
 	    sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} rm /home/TheFantasticDevSecOps/ -r"
             sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} /home/nuclei -t /root/nuclei-templates/file -target /home/${projname} -o /home/nuclei-results.txt"
+	    sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} chmod 744 /home/nuclei-results.txt"
             sh "scp -P ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP}:/home/nuclei-results.txt /home"
           
           //Migrar regexScanner.
