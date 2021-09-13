610882f105ec60474403c15342fa2aafbaa1378f
Kalust <39742600+Kalust@users.noreply.github.com>
2021-09-13 12:15:13 -0300

    Update parseNucleiResults.py

--- SAST/NucleiResultsParser/parseNucleiResults.py
+++ SAST/NucleiResultsParser/parseNucleiResults.py
@@ -10,44 +10,6 @@ sourcePath = sys.argv[1]
 outputPath = sys.argv[2]
 nucleiFinalJson = []
 
-def jsonLoad():
-    try:
-        with open(sourcePath) as nucleiTxt:
-            data = nucleiTxt.read()
-            data = data.replace('}\n{', '},{')
-            data = "["+data.__add__("]")
-            nucleiResults = json.loads(data) 
-        return nucleiResults
-    except:
-        print("Error al cargar el resultado de nuclei: ",sys.exc_info()[0])
-        raise
-
-def nucleiJsonParse():
-    print("------------------------------------------")
-    print("New Nuclei JSON Parsing \n")
-    print("Date: " + datetime.datetime.now().strftime("%d/%m/%Y - %H:%M:%S")+"\n")
-    nucleiResults = jsonLoad()
-    if(nucleiResults is not None):
-        try:
-            for issue in nucleiResults:
-                nucleiTitle = issue["templateID"]
-                nucleiComponent = issue["host"]
-                nucleiSeverity = issue["info"]["severity"]
-                nucleiAffectedCode = issue["extracted_results"]
-                nucleiJson = {
-                    'title': issue["templateID"],
-                    'component': issue["host"],
-                    'severity': issue["info"]["severity"],
-                    'affectedCode' : issue["extracted_results"]
-                }
-                nucleiFinalJson.append(nucleiJson)
-            outputNucleiResults(nucleiFinalJson)
-        except:
-            print("Error: no se pudo parsear.",sys.exc_info()[0])
-            pass
-    else:
-        sys.exit(5)
-   
 def outputNucleiResults(nucleiFinalJson):
     try:
         with open(outputPath,'w') as nucleiJSON:
@@ -57,4 +19,47 @@ def outputNucleiResults(nucleiFinalJson):
         print("Error: no se pudo escribir el resultado.")
         pass
 
-nucleiJsonParse()
+def initParser():
+    print("------------------------------------------")
+    print("New Nuclei JSON Parsing \n")
+    print("Date: " + datetime.datetime.now().strftime("%d/%m/%Y - %H:%M:%S")+"\n")
+    fileJSON = open(sourcePath,'r')
+    return fileJSON
+
+def parseJSON():
+    try:
+        fileJSON = initParser()      
+        for line in fileJSON.readlines():
+            if "}{" in line:
+                brokenJSON = line.split("}{")
+                brokenJSON[0] += "}"
+                brokenJSON[1] = "{" + brokenJSON[1]
+                if 'Z"}' not in brokenJSON[1]:
+                    brokenJSON[1] += "}"
+                loadJSON(brokenJSON[0])
+                loadJSON(brokenJSON[1])           
+            else:
+                loadJSON(line)
+        outputNucleiResults(nucleiFinalJson)
+    except:
+        print("Error: "+ sys.exc_info()[0])
+        pass
+
+def loadJSON(line):
+    try:
+        issue = json.loads(line)
+        nucleiTitle = issue["templateID"]
+        nucleiComponent = issue["host"]
+        nucleiSeverity = issue["info"]["severity"]
+        nucleiAffectedCode = issue["extracted_results"]
+        nucleiJson = {
+            'title': issue["templateID"],
+            'component': issue["host"],
+            'severity': issue["info"]["severity"],
+            'affectedCode' : issue["extracted_results"]
+        }
+        nucleiFinalJson.append(nucleiJson) 
+    except:
+        print("Linea fallida:\n" + line)
+
+parseJSON()
