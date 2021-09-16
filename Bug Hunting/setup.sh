f4038b09e08ec75f68d7f656df498b900890035a
Maxpowersi <maxi1990@gmail.com>
2021-09-16 18:53:10 -0300

    Update setup.sh

--- Bug Hunting/setup.sh
+++ Bug Hunting/setup.sh
@@ -34,6 +34,15 @@ checkprogram()
 }
 
 sudo apt install golang-go
+sudo apt-get install nikto -y
+sudo apt-get install sslscan
+sudo apt install testssl.sh
+sudo npm install -g yarn
+git clone https://github.com/aliasio/wappalyzer
+cd wappalyzer
+yarn install
+yarn run link
+cd ..
 
 checkprogram "nuclei"
 checkprogram "brew"
