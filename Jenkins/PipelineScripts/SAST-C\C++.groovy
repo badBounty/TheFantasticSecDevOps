b817127e58aef6f9a013f16c6278c25d34629f31
Kalust <39742600+Kalust@users.noreply.github.com>
2021-09-16 12:00:21 -0300

    Create SAST-C\C++.groovy

---
+++ Jenkins/PipelineScripts/SAST-C\C++.groovy
@@ -0,0 +1,42 @@
+import groovy.json.JsonSlurperClassic
+
+def runStage(notifier, vulns)
+{
+    def projname = env.JOB_NAME
+    try 
+    {
+        notifier.sendMessage('','good','Stage: "SAST-C\C++": INIT')
+	
+        sshagent(['ssh-key-SAST-image']) 
+        {
+          sh "ssh-keygen -f '/var/jenkins_home/.ssh/known_hosts' -R [${env.SAST_Server_IP}]:${env.SAST_Server_SSH_Port}"
+          sh "scp -P ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no -v -r \$(pwd) root@${env.SAST_Server_IP}:/home"
+          
+          /*
+          
+          Se deberían correr las tools correspondientes luego de copiar el repo remoto a SAST image.
+          También, un parser para el output de las tools.
+          
+          */
+        }	
+	
+        //sh """sed -i -e 's/\\/home\\/${projname}\\///g' nucleiParsedResults.json"""
+
+        /*
+        
+        Se debe agregar al array de vulns los findings en formato JSON.
+        
+        */
+		    
+        notifier.sendMessage('','good','Stage: "SAST-C\C++": SUCCESS')
+    }
+    catch(Exception e) 
+    {
+        notifier.sendMessage('','danger','Stage: "SAST-C\C++": FAILURE')	
+        currentBuild.result = 'FAILURE'
+	      print('Stage: "SAST-C\C++": FAILURE')
+        print(e.getMessage())
+    }
+}
+
+return this
