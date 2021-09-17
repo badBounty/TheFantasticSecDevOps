3c9d8ded432c6b519c1da49c6477ebd26dd6b84a
Kalust <39742600+Kalust@users.noreply.github.com>
2021-09-17 12:24:01 -0300

    Update SAST-C-CPP.groovy

--- Jenkins/PipelineScripts/SAST-C-CPP.groovy
+++ Jenkins/PipelineScripts/SAST-C-CPP.groovy
@@ -11,8 +11,8 @@ def runStage(notifier, vulns)
         {
           sh "ssh-keygen -f '/var/jenkins_home/.ssh/known_hosts' -R [${env.SAST_Server_IP}]:${env.SAST_Server_SSH_Port}"
           sh "scp -P ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no -v -r \$(pwd) root@${env.SAST_Server_IP}:/home"
-	  sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} flawfinder -c -D --csv /home/${projname} > ${projname}-flawfinder.csv"
-	  sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} python3 /home/parseFlawfinderResults.py /home/${projname}-flawfinder.csv /home/flawfinder-results-parsed.json ${projname}"
+	  sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} flawfinder -c -D --csv /home/${projname} > flawfinder.csv"
+	  sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} python3 /home/parseFlawfinderResults.py flawfinder.csv /home/flawfinder-results-parsed.json ${projname}"
           sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} rm /home/${projname}-flawfinder.csv"	
           sh "scp -P ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP}:/home/flawfinder-results-parsed.json ./flawfinderParsedResults.json"
         }	
