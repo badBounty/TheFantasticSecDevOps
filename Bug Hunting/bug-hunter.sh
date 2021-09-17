e2056c23cdcfc84431d18a87e85db038ac6695cd
cangaruu <lenaquemad4@gmail.com>
2021-09-17 16:39:26 -0400

    web_scanner call in bug-hunter

--- Bug Hunting/bug-hunter.sh
+++ Bug Hunting/bug-hunter.sh
@@ -9,5 +9,12 @@ do
   sudo ./nmap_scan.sh subdomains_np.txt $2
   sudo ./nuclei_scan.sh subdomains.txt $2
   sudo ./dirnfiles_enum.sh subdomains.txt $2
+
+  IFS=$'\n'
+  set -f
+  for domain in $(cat < $1); do
+  	sudo./Web\ Scanner/web_scanner.sh $domain $2
+  done
+
   echo "bug-hunter - done" | slackcat -c $2 -s
 done
