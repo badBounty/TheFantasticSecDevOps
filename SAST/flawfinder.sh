5ce529f2db333d260f852452e2e98d7a107867f5
Kalust <39742600+Kalust@users.noreply.github.com>
2021-09-20 09:50:39 -0300

    Create flawfinder.sh

---
+++ SAST/flawfinder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+if [ "$#" != "1" ] ; then
+  echo "Se esperaban 1 argumento y se recibieron $#";
+  exit 1;
+fi
+
+flawfinder -c -D --csv /home/$1 > flawfinder.csv
+
+python3 /home/parseFlawfinderResults.py /home/flawfinder.csv /home/flawfinder-results-parsed.json $1"
