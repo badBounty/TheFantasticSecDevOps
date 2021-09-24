6569ea94753953cb5b05c9f16a28322991b3d0f0
Kalust <39742600+Kalust@users.noreply.github.com>
2021-09-24 10:52:07 -0300

    Update parseNodejsscan.py

--- SAST/parseNodejsscan.py
+++ SAST/parseNodejsscan.py
@@ -1,7 +1,7 @@
 import re
 import json
 import sys
-
+import base64
 
 def parser(fname, to):
     j = json.load(open(fname, 'r'))
@@ -9,11 +9,12 @@ def parser(fname, to):
     vulns = j["nodejs"]
     for vuln in vulns:
         for f in vulns[vuln]["files"]:
+            line = base64.b64encode(bytes(f["match_string"], 'utf-8'))
             issue = {
                 "title": vuln,
                 "lineNumber": f["match_lines"][0],
                 "file": f["file_path"],
-                "line": f["match_string"]
+                "line": line
             }
             print(issue)
             issues.append(issue)
