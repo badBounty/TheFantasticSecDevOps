70ead8df8791144d9defc3fe4732f88c91577aed
Kalust <39742600+Kalust@users.noreply.github.com>
2021-10-01 13:34:57 -0300

    Update PipelineTemplate-NodeJS.groovy

--- Jenkins/PipelineScripts/PipelineTemplate-NodeJS.groovy
+++ Jenkins/PipelineScripts/PipelineTemplate-NodeJS.groovy
@@ -84,6 +84,7 @@ pipeline
                         modules.SAST_SonarResults = load "Jenkins/PipelineScripts/SAST-SonarResults.groovy"
                         modules.SAST_NodeJS = load "Jenkins/PipelineScripts/SAST-NodeJS.groovy"
                         modules.SAST_NPMAudit = load "Jenkins/PipelineScripts/SAST-NodeJS-NPMAudit.groovy"
+                        modules.SAST_SCA = load "Jenkins/PipelineScripts/SAST-SCA-NodeJS.groovy"
                         modules.SAST_Dependencies = load "Jenkins/PipelineScripts/SAST-DependencyCheck.groovy"
                         modules.SAST_Destroy = load "Jenkins/PipelineScripts/SAST-Destroy.groovy"
                         modules.SAST_Nuclei = load "Jenkins/PipelineScripts/SAST-Nuclei.groovy"
@@ -208,6 +209,23 @@ pipeline
                     }
                     
                     modules.SAST_NPMAudit.runStage(modules.Notifier, vulns)
+
+                }
+            }
+        }
+        
+        stage('SAST-SCA-NodeJS')
+        {
+            steps
+            {
+                script
+                {
+                    if (SkipBuild == 'YES'){
+                        currentBuild.result = 'SUCCESS'
+                        return
+                    }
+                    
+                    modules.SAST_SCA.runStage(modules.Notifier, vulns)
 
                 }
             }
