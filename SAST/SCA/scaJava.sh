8fb0d3cd4a5cff1bc2fa2f4bfd70f38ddabec8c1
Kalust <39742600+Kalust@users.noreply.github.com>
2021-10-04 12:29:30 -0300

    Delete scaJava.sh

--- SAST/SCA/scaJava.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x
-if [ "$#" != "1" ] ; then
-  echo "Se esperaba 1 argumento y se recibieron $#";
-  exit 1;
-fi
-
-mvn dependency:tree -DoutputType=dot | grep \> | cut -d\> -f2 > scaMaven-$1.txt
