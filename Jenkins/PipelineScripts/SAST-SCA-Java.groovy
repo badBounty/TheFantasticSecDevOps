c1c238c96e3d844a2d6ce55cbac0f5c0cb03b23b
Kalust <39742600+Kalust@users.noreply.github.com>
2021-10-04 10:08:18 -0300

    Update SAST-SCA-Java.groovy

--- Jenkins/PipelineScripts/SAST-SCA-Java.groovy
+++ Jenkins/PipelineScripts/SAST-SCA-Java.groovy
@@ -6,8 +6,9 @@ def runStage(notifier)
     try 
     {
         notifier.sendMessage('','good','Stage: "SAST-SCA-Java": INIT')
-
-        def results = sh(script: "mvn dependency:tree -DoutputType=dot && grep \> && cut -d\> -f2", returnStdout: true)
+	sh "ssh-keygen -f '/var/jenkins_home/.ssh/known_hosts' -R [${env.SAST_Server_IP}]:${env.SAST_Server_SSH_Port}"
+	sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} /home/scaJava.sh ${projname}"
+        def results = sh(script: "cat ./scaMaven-${projname}.txt" , returnStdout: true)
         print('Maven Libraries: \n')
         print('Format --> Group:Artifact:Type:Version:Scope \n')
         print(results)
