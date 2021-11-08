8bcd9d0b0917d9a8658593c12d786e6516b87e6d
Kalust <39742600+Kalust@users.noreply.github.com>
2021-11-08 15:16:55 -0300

    Update SAST-Cloning.groovy

--- Jenkins/PipelineScripts/SAST-Cloning.groovy
+++ Jenkins/PipelineScripts/SAST-Cloning.groovy
@@ -1,5 +1,6 @@
 def runStage(notifier)
 {
+    def projname = env.JOB_NAME
     try 
     {
         notifier.sendMessage('','good','Stage: "SAST-Cloning": INIT')
@@ -7,6 +8,7 @@ def runStage(notifier)
         sshagent(['ssh-key-SAST-image']) 
         {
           sh "ssh-keygen -f '/var/jenkins_home/.ssh/known_hosts' -R [${env.SAST_Server_IP}]:${env.SAST_Server_SSH_Port}"
+    	  sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} rm -rf /home/${projname}"
           sh "scp -P ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no -v -r \$(pwd) root@${env.SAST_Server_IP}:/home"
         }	
 	
