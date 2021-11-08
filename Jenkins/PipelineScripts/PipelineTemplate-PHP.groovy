a97d59c6d7807cf03a01475874847b0a0a24de0d
Kalust <39742600+Kalust@users.noreply.github.com>
2021-11-08 15:32:13 -0300

    Update PipelineTemplate-PHP.groovy

--- Jenkins/PipelineScripts/PipelineTemplate-PHP.groovy
+++ Jenkins/PipelineScripts/PipelineTemplate-PHP.groovy
@@ -84,6 +84,7 @@ pipeline {
                         modules.SAST_SonarResults = load "Jenkins/PipelineScripts/SAST-SonarResults.groovy"
                         modules.SAST_Dependencies = load "Jenkins/PipelineScripts/SAST-DependencyCheck.groovy"
                         modules.SAST_Nuclei = load "Jenkins/PipelineScripts/SAST-Nuclei.groovy"
+                        modules.SAST_Cloning = load "Jenkins/PipelineScripts/SAST-Cloning.groovy"
                         modules.SAST_Destroy = load "Jenkins/PipelineScripts/SAST-Destroy.groovy"
                         modules.SAST_PostResults = load "Jenkins/PipelineScripts/SAST-PostResults.groovy"
                         modules.SAST_SendVulnsLog = load "Jenkins/PipelineScripts/SAST-SendVulnsLog.groovy"
@@ -124,6 +125,18 @@ pipeline {
                         return
                     }
                     modules.SAST_Deployment.runStage(modules.Notifier)
+                }
+            }
+        }
+        
+        stage('SAST-Cloning'){
+            steps{
+                script{
+                    if (SkipBuild == 'YES'){
+                        currentBuild.result = 'SUCCESS'
+                        return
+                    }
+                    modules.SAST_Cloning.runStage(modules.Notifier)
                 }
             }
         }
