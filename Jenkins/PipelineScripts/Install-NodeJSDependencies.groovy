8993a05b7e61c57260bd8fb25d7df1cd61d5cefa
Kalust <39742600+Kalust@users.noreply.github.com>
2021-11-09 12:42:23 -0300

    Update Install-NodeJSDependencies.groovy

--- Jenkins/PipelineScripts/Install-NodeJSDependencies.groovy
+++ Jenkins/PipelineScripts/Install-NodeJSDependencies.groovy
@@ -3,12 +3,11 @@ def runStage(notifier)
 	try 
 	{
 		def emailPrivateRepo = env.EmailPrivateRepo
-		print(emailPrivateRepo)
 		notifier.sendMessage('','good','Stage: "Install-Dependencies": INIT')
 		
 		withCredentials([usernamePassword(credentialsId: 'git-code-token-nodeJS', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')])
 		{
-			sh 'npm-cli-login -u ${USERNAME} -p {PASSWORD} -e ${emailPrivateRepo}'
+			sh 'npm-cli-login -u ${USERNAME} -p {PASSWORD} -e "eberruttomorales@deloitte.com"'
 		}
 		
 		sh 'npm install --force'
