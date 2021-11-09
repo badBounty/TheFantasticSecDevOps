ee2c74a0e6e6d010fce66bba0a1ec5b0f93656d1
Kalust <39742600+Kalust@users.noreply.github.com>
2021-11-09 10:49:13 -0300

    Create DependenciesReplaceNodeJS.groovy

---
+++ Jenkins/PipelineScripts/DependenciesReplaceNodeJS.groovy
@@ -0,0 +1,24 @@
+def runStage(notifier)
+{
+    def projname = env.JOB_NAME
+    try
+    {
+        modules.Notifier.sendMessage('','good','Stage: "Dependencies-Replace-NodeJS": INIT')
+
+        withCredentials([usernamePassword(credentialsId: 'git-code-token-nodeJS', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')])
+        {
+          sh 'sed -i "s/bitbucket/${USERNAME}:${PASSWORD}@bitbucket/g" package.json'
+        }
+
+        modules.Notifier.sendMessage('','good','Stage: "Dependencies-Replace-NodeJS": SUCCESS')
+        print('Stage: "Dependencies-Replace-NodeJS": SUCCESS')
+    }
+    catch(Exception e)
+    {
+        modules.Notifier.sendMessage('','danger','Stage: "Dependencies-Replace-NodeJS": FAILURE')
+        currentBuild.result = 'FAILURE'
+        print('Stage: "Dependencies-Replace-NodeJS": FAILURE')
+    } 
+}
+
+return this
