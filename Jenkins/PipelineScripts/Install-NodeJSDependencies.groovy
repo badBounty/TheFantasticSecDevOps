efb31ec73a18ab9772a1864ffd5aced3fa07ab4c
Kalust <39742600+Kalust@users.noreply.github.com>
2021-11-09 12:44:33 -0300

    Update Install-NodeJSDependencies.groovy

--- Jenkins/PipelineScripts/Install-NodeJSDependencies.groovy
+++ Jenkins/PipelineScripts/Install-NodeJSDependencies.groovy
@@ -7,7 +7,7 @@ def runStage(notifier)
 		
 		withCredentials([usernamePassword(credentialsId: 'git-code-token-nodeJS', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')])
 		{
-			sh 'npm-cli-login -u ${USERNAME} -p {PASSWORD} -e "eberruttomorales@deloitte.com"'
+			sh 'npm-cli-login -u ${USERNAME} -p "Holass11%21" -e "eberruttomorales@deloitte.com"'
 		}
 		
 		sh 'npm install --force'
