26dca4f3207664e48b6309d3812b6befabeb3542
Kalust <39742600+Kalust@users.noreply.github.com>
2021-11-09 11:57:37 -0300

    Update DependenciesReplaceNodeJS.groovy

--- Jenkins/PipelineScripts/DependenciesReplaceNodeJS.groovy
+++ Jenkins/PipelineScripts/DependenciesReplaceNodeJS.groovy
@@ -1,21 +1,20 @@
 def runStage(notifier)
 {
-    def projname = env.JOB_NAME
     try
     {
-        modules.Notifier.sendMessage('','good','Stage: "Dependencies-Replace-NodeJS": INIT')
+        notifier.sendMessage('','good','Stage: "Dependencies-Replace-NodeJS": INIT')
 
         withCredentials([usernamePassword(credentialsId: 'git-code-token-nodeJS', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')])
         {
             sh 'sed -i "s/bitbucket/${USERNAME}:${PASSWORD}@bitbucket/g" package.json'
         }
 
-        modules.Notifier.sendMessage('','good','Stage: "Dependencies-Replace-NodeJS": SUCCESS')
+        notifier.sendMessage('','good','Stage: "Dependencies-Replace-NodeJS": SUCCESS')
         print('Stage: "Dependencies-Replace-NodeJS": SUCCESS')
     }
     catch(Exception e)
     {
-        modules.Notifier.sendMessage('','danger','Stage: "Dependencies-Replace-NodeJS": FAILURE')
+        notifier.sendMessage('','danger','Stage: "Dependencies-Replace-NodeJS": FAILURE')
         currentBuild.result = 'FAILURE'
         print('Stage: "Dependencies-Replace-NodeJS": FAILURE')
     } 
