0d0f38b905ebc982b1c8400e2ff933b69823dda6
Kalust <39742600+Kalust@users.noreply.github.com>
2021-11-12 16:05:37 -0300

    Update SAST-NodeJS-NPMAudit.groovy

--- Jenkins/PipelineScripts/SAST-NodeJS-NPMAudit.groovy
+++ Jenkins/PipelineScripts/SAST-NodeJS-NPMAudit.groovy
@@ -14,12 +14,12 @@ def runStage(notifier, vulns)
             def resultNPMAudit = sh(script: "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} npm --prefix /home/${pathPackageLockJson} audit --json", returnStdout:true)
             writeFile(file: 'npmaudit.json', text: resultNPMAudit)
 	        sh "scp -P ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no ./npmaudit.json root@${env.SAST_Server_IP}:/home/npmaudit.json"
-            sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} python3 /home/parseNPMAuditResults.py /home/npmaudit.json /home/output.json /home/severity.txt"
-            sh "scp -P ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP}:/home/output.json ./output.json"
-            sh "scp -P ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP}:/home/severity.txt ./severity.txt"
-            sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} rm /home/output.json"
+            //sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} python3 /home/parseNPMAuditResults.py /home/npmaudit.json /home/output.json /home/severity.txt"
+            //sh "scp -P ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP}:/home/output.json ./output.json"
+            //sh "scp -P ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP}:/home/severity.txt ./severity.txt"
+            //sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} rm /home/output.json"
         }
-
+	/*
         def results = sh(script: "cat output.json", returnStdout: true).trim()
         def severity = sh(script: "cat severity.txt", returnStdout: true).trim()
         results = results.replace("\\", "")
@@ -29,7 +29,7 @@ def runStage(notifier, vulns)
             severity = "High"
         }
         vulns.add(["Outdated 3rd Party libraries", results, projname, 0, projname, "null", severity, "NPM-Audit"])
-        
+        */
         notifier.sendMessage('','good','Stage: "SAST-NodeJS-NPMAudit": SUCCESS')
     }
     catch(Exception e) 
