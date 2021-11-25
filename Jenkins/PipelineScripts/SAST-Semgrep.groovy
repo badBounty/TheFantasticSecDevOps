acca193790e064a0169da45da6f5cb81d7884d44
Kalust <39742600+Kalust@users.noreply.github.com>
2021-11-25 13:12:10 -0300

    Update SAST-Semgrep.groovy

--- Jenkins/PipelineScripts/SAST-Semgrep.groovy
+++ Jenkins/PipelineScripts/SAST-Semgrep.groovy
@@ -16,39 +16,31 @@ def runStage(notifier, vulns)
 	    sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} git clone https://github.com/returntocorp/semgrep-rules"
 	    sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} mv semgrep-rules /home"
 	    sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} semgrep --config /home/semgrep-rules/${semgrepRule}/ --config /home/semgrep-rules/generic/secrets/security/ /home/${projname}/ -o /home/semgrep${projname}.json --json --skip-unknown-extensions --verbose"
-	    sh "scp -P ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP}:/home/semgrep${projname}.json ./semgrepResults.json"
+	    sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} python3 /home/SemgrepParser.py /home/semgrep${projname}.json /home/semgrepParsed.json ${projname}"
+	    sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} rm /home/semgrep${projname}.json"	
+            sh "scp -P ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP}:/home/semgrepParsed.json ./semgrepParsedResults.json"
 		
-	    //SIEMPRE ACTUALIZAR SEMGREP. Algunas rules no se pueden parsear si no está en la ultima version.
-            //Clonar registry rules de semgrep.
-            //Ver la forma de whitelistear o blacklistear.
-            //Correr semgrep.
-            //Guardar en un output y parsear.
-            
-	    //Falta el parsing.
-            
+	    //SIEMPRE ACTUALIZAR SEMGREP. Algunas rules no se pueden parsear si no está en la ultima version.     
         }	
 
-        def results = sh(script: "cat ./semgrepResults.json", returnStdout: true).trim()
-	//JsonOutput.prettyPrint(results)
+        def results = sh(script: "cat ./semgrepParsedResults.json", returnStdout: true).trim()
 	
-	/*
-        def json = new JsonSlurperClassic().parseText(results)
+	def json = new JsonSlurperClassic().parseText(results)
         results = null
         	
         json.each{issue ->
             def title = issue["title"]
             def message = issue["title"]
-	          def component = issue["component"]
+	    def component = issue["component"]
             def sev = issue["severity"]
-            def line = "N/A"
-            def affected_code = issue["affectedCode"]
-            def hash = sh(returnStdout: true, script: "sha256sum \$(pwd)/${component} | awk 'NR==1{print \$1}'")    
+	    def line = issue["line"]
+	    def affected_code = issue["affectedCode"]
+	    def hash = sh(returnStdout: true, script: "sha256sum \$(pwd)/${component} | awk 'NR==1{print \$1}'")    
             hash = hash.replace("\n", " ")
-            if (title.matches("[a-zA-Z0-9].*")){
-               vulns.add([title, message, component, line, affected_code, hash, sev, "Nuclei"])
-            }
+	    if (title.matches("[a-zA-Z0-9].*")){
+		vulns.add([title, message, component, line, affected_code, hash, sev, "Semgrep"])
+	    }
         }
-		    */
       
         notifier.sendMessage('','good','Stage: "SAST-Semgrep": SUCCESS')
     }
