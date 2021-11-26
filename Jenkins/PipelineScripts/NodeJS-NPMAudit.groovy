275bf600fe3788a34b0f3c4af8de8a620d621a66
Kalust <39742600+Kalust@users.noreply.github.com>
2021-11-26 14:15:55 -0300

    Update NodeJS-NPMAudit.groovy

--- Jenkins/PipelineScripts/NodeJS-NPMAudit.groovy
+++ Jenkins/PipelineScripts/NodeJS-NPMAudit.groovy
@@ -4,7 +4,7 @@ def runStage(notifier, vulns)
 	
     try 
     {
-        notifier.sendMessage('','good','Stage: "SAST-NodeJS-NPMAudit": INIT')
+        notifier.sendMessage('','good','Stage: "NodeJS-NPMAudit": INIT')
 
         def projname = env.JOB_NAME
 	    
@@ -23,13 +23,13 @@ def runStage(notifier, vulns)
         vulns.add(["Outdated 3rd Party libraries", results, projname, 0, projname, "null", severity, "NPM-Audit"])
         */
 	    
-        notifier.sendMessage('','good','Stage: "SAST-NodeJS-NPMAudit": SUCCESS')
+        notifier.sendMessage('','good','Stage: "NodeJS-NPMAudit": SUCCESS')
     }
     catch(Exception e) 
     {
-        notifier.sendMessage('','danger','Stage: "SAST-NodeJS-NPMAudit": FAILURE')
+        notifier.sendMessage('','danger','Stage: "NodeJS-NPMAudit": FAILURE')
         currentBuild.result = 'FAILURE'
-        print('Stage: "SAST-NodeJS-NPMAudit": FAILURE')
+        print('Stage: "NodeJS-NPMAudit": FAILURE')
         print(e.getMessage())
     }
 }
