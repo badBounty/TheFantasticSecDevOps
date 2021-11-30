bf0b36ec540f6d23c10f2ee4bb941730f13c1fff
Kalust <39742600+Kalust@users.noreply.github.com>
2021-11-30 14:59:17 -0300

    Update SAST-NodeJS-NPMAudit.groovy

--- Jenkins/PipelineScripts/SAST-NodeJS-NPMAudit.groovy
+++ Jenkins/PipelineScripts/SAST-NodeJS-NPMAudit.groovy
@@ -14,16 +14,16 @@ def runStage(notifier, vulns)
         {
           sh "ssh-keygen -f '/var/jenkins_home/.ssh/known_hosts' -R [${env.SAST_Server_IP}]:${env.SAST_Server_SSH_Port}"
           sh "scp -P ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no -v -r \$(pwd)/npmAudit.json root@${env.SAST_Server_IP}:/home/npmAudit.json"
-          sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} python3 /home/parseNPMAuditResults.py /home/npmAudit.json /home/npmAudit.json /home/severity.txt"
-          sh "scp -P ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP}:/home/npmAudit.json ./NPMAuditParsed.json"	
+          //sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} python3 /home/parseNPMAuditResults.py /home/npmAudit.json /home/npmAudit.json /home/severity.txt"
+          //sh "scp -P ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP}:/home/npmAudit.json ./NPMAuditParsed.json"	
         }    
-	   	
+	/*   	
         def results = sh(script: "cat NPMAuditParsed.json", returnStdout: true).trim()
         results = results.replace("\\", "")
         results = results.replace("\"", "\\\"")
         results = results.replace("\n", " ")
         vulns.add(["Outdated 3rd Party libraries", results, projname, 0, projname, "null", severity, "NPM-Audit"])
-        
+        */
 	    
         notifier.sendMessage('','good','Stage: "SAST-NPMAudit": SUCCESS')
     }
