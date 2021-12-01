853736d3720a56fa2d079580f035b7e364e6cc15
Kalust <39742600+Kalust@users.noreply.github.com>
2021-12-01 12:55:09 -0300

    Update SAST-SonarQube-Dotnet.groovy

--- Jenkins/PipelineScripts/SAST-SonarQube-Dotnet.groovy
+++ Jenkins/PipelineScripts/SAST-SonarQube-Dotnet.groovy
@@ -10,9 +10,9 @@ def runStage(notifier)
         {
             sshagent(['ssh-key-SAST-image']) 
             {
-                sh "/root/.dotnet/tools/dotnet-sonarscanner begin /k:${projname} /d:sonar.login=${USERNAME} /d:sonar.password=${PASSWORD} /d:sonar.host.url=http://${env.SAST_Server_IP}:${env.Sonar_Port}"
-                sh 'find . -name *.sln -exec dotnet build {} ";"'
-                sh "/root/.dotnet/tools/dotnet-sonarscanner end /d:sonar.login=${USERNAME} /d:sonar.password=${PASSWORD}"
+                sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} /root/.dotnet/tools/dotnet-sonarscanner begin /k:${projname} /d:sonar.login=${USERNAME} /d:sonar.password=${PASSWORD} /d:sonar.host.url=http://${env.SAST_Server_IP}:${env.Sonar_Port}"
+                sh 'ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} find . -name *.sln -exec dotnet build {} ";"'
+                sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} /root/.dotnet/tools/dotnet-sonarscanner end /d:sonar.login=${USERNAME} /d:sonar.password=${PASSWORD}"
             }
         }
 
