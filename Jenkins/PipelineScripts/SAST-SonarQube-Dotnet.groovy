37e91ad375cf42000d15cce038d762fa3fda1791
Kalust <39742600+Kalust@users.noreply.github.com>
2021-12-01 16:11:50 -0300

    Update SAST-SonarQube-Dotnet.groovy

--- Jenkins/PipelineScripts/SAST-SonarQube-Dotnet.groovy
+++ Jenkins/PipelineScripts/SAST-SonarQube-Dotnet.groovy
@@ -8,12 +8,9 @@ def runStage(notifier)
         def projname = env.JOB_NAME
         withCredentials([usernamePassword(credentialsId: 'sonar-credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')])
         {
-            sshagent(['ssh-key-SAST-image']) 
-            {
-                sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} /root/.dotnet/tools/dotnet-sonarscanner begin /k:${projname} /d:sonar.login=${USERNAME} /d:sonar.password=${PASSWORD} /d:sonar.host.url=http://${env.SAST_Server_IP}:${env.Sonar_Port}"
-                sh 'ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} find . -name *.sln -exec dotnet build {} ";"'
-                sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} /root/.dotnet/tools/dotnet-sonarscanner end /d:sonar.login=${USERNAME} /d:sonar.password=${PASSWORD}"
-            }
+            sh "/root/.dotnet/tools/dotnet-sonarscanner begin /k:${projname} /d:sonar.login=${USERNAME} /d:sonar.password=${PASSWORD} /d:sonar.host.url=http://${env.SAST_Server_IP}:${env.Sonar_Port}"
+            sh 'find . -name *.sln -exec dotnet build {} ";"'
+            sh "/root/.dotnet/tools/dotnet-sonarscanner end /d:sonar.login=${USERNAME} /d:sonar.password=${PASSWORD}"
         }
 
         notifier.sendMessage('','good','Stage: "SAST-SonarQube": SUCESS')
