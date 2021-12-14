744253f6843c5ce94ff0d24df060d6f92fee6d57
Kalust <39742600+Kalust@users.noreply.github.com>
2021-12-14 13:04:38 -0300

    Update SAST-Cloning.groovy

--- Jenkins/PipelineScripts/SAST-Cloning.groovy
+++ Jenkins/PipelineScripts/SAST-Cloning.groovy
@@ -1,7 +1,7 @@
 def runStage(notifier)
 {
     def projname = env.JOB_NAME
-    def custGit = env.CustomGit
+    boolean custGit = env.CustomGit
     try 
     {
         notifier.sendMessage('','good','Stage: "SAST-Cloning": INIT')
@@ -10,7 +10,8 @@ def runStage(notifier)
         {
           sh "ssh-keygen -f '/var/jenkins_home/.ssh/known_hosts' -R [${env.SAST_Server_IP}]:${env.SAST_Server_SSH_Port}"
     	  sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} rm -rf /home/${projname}"
-	  if(custGit.equals(true)){
+	  print(custGit)
+	  if(custGit == true){
 		  print("custom git is true")
 	      sh "scp -P ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no -v -r \$(pwd)/${projname} root@${env.SAST_Server_IP}:/home"
 	  }
