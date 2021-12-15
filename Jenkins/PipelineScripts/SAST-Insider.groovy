56d76ecc790901f96d8d8df07f952a9d2b480ec3
Kalust <39742600+Kalust@users.noreply.github.com>
2021-12-15 10:38:56 -0300

    Update SAST-Insider.groovy

--- Jenkins/PipelineScripts/SAST-Insider.groovy
+++ Jenkins/PipelineScripts/SAST-Insider.groovy
@@ -3,6 +3,7 @@ import groovy.json.JsonSlurperClassic
 def runStage(notifier, vulns)
 {
     def projname = env.JOB_NAME
+    def insiderTech = env.InsiderTechnology
     try 
     {
         notifier.sendMessage('','good','Stage: "SAST-Insider": INIT')
@@ -12,12 +13,14 @@ def runStage(notifier, vulns)
             sh "ssh-keygen -f '/var/jenkins_home/.ssh/known_hosts' -R [${env.SAST_Server_IP}]:${env.SAST_Server_SSH_Port}"
             sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} cd /home"
             sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} mv /opt/sonarqube/insider /home"
-            sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} /home/insider "
-            sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} python3 /home/parseInsiderResults.py"
+            sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} /home/insider --tech ${insiderTech} --target /home/${projname}"
+            //sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} python3 /home/parseInsiderResults.py"
             //sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} rm /home/nuclei-results.txt"	
-            sh "scp -P ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP}:/home/nuclei-results-parsed.json ./nucleiParsedResults.json"
+            //sh "scp -P ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP}:/home/nuclei-results-parsed.json ./nucleiParsedResults.json"
         }	
-
+	
+	/*    
+	    
         def results = sh(script: "cat ./insiderParsedResults.json", returnStdout: true).trim()
         def json = new JsonSlurperClassic().parseText(results)
         results = null
@@ -35,6 +38,8 @@ def runStage(notifier, vulns)
               vulns.add([title, message, component, line, affected_code, hash, sev, "Insider"])
             }
         }
+	
+	*/
 		    
         notifier.sendMessage('','good','Stage: "SAST-Insider": SUCCESS')
     }
