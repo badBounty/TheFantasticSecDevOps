1191787f801526266f77967415dca36029a2d14c
Kalust <39742600+Kalust@users.noreply.github.com>
2022-01-04 12:03:02 -0300

    Update SAST-SonarResults.groovy

--- Jenkins/PipelineScripts/SAST-SonarResults.groovy
+++ Jenkins/PipelineScripts/SAST-SonarResults.groovy
@@ -5,42 +5,34 @@ def runStage(notifier, vulns)
     try
     {
         notifier.sendMessage('','good','Stage: "SAST-SonarResults": INIT')
-
-        def response = httpRequest "http://${env.SAST_Server_IP}:${env.Sonar_Port}/api/issues/search?p=1"
-            print(response.status)
-            def json = new JsonSlurperClassic().parseText(response.content)
-            
-            def GIT_COMMIT = sh(returnStdout: true, script: 'git rev-parse HEAD').take(7)
-            def GIT_MAIL = sh(returnStdout: true, script: 'git show -s --format=%ae').trim()
-            def projname = env.JOB_NAME
-            json.issues.each{issue ->
-                if (issue.type == 'VULNERABILITY' & issue.status == 'OPEN'){
-                    print(issue)
-                    def sev = issue.severity
-                    def title = issue.rule
-                    def message = issue.message.replaceAll('"', "'")
-                    if (title.matches("[a-zA-Z0-9].*")){
-                        def hash = issue.hash
-                        def component = issue.component
-                        if (issue.component.contains(":")){
-                            component = issue.component.split(":").last()
-                        }
-                        
-                        def line = issue.line
-                        def affected_code = ""
-                        try{
-                            affected_code = sh(returnStdout: true, script: "sed '${line}!d' ${component}")
-                        }
-                        catch (Exception except)
-                        {
-                            affected_code = "none"
-                        }
-                        
-                        def date = issue.updateDate.split('T')[0]
-                        vulns.add([title, message, component, line, affected_code, hash, sev, "SonarQube"])
-                    }
-                }
+	
+	def projname = env.JOB_NAME
+        withCredentials([usernamePassword(credentialsId: 'sonar-credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')])
+        {       
+            sshagent(['ssh-key-SAST-image']) 
+            {
+                sh "ssh -p ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP} python3 /home/SonarResults.py ${USERNAME} ${PASSWORD} http://localhost ${env.Sonar_Port} /home/SonarResultsParsed.json"
+		sh "scp -P ${env.SAST_Server_SSH_Port} -o StrictHostKeyChecking=no root@${env.SAST_Server_IP}:/home/SonarResultsParsed.json ./SonarResultsParsed.json"    
             }
+        }    
+	 
+	def results = sh(script: "cat ./nucleiParsedResults.json", returnStdout: true).trim()
+        def json = new JsonSlurperClassic().parseText(results)
+        results = null  
+	    
+        json.each{issue ->
+            def title = issue["title"]
+            def message = issue["title"]
+	    def component = issue["component"]
+            def sev = issue["severity"]
+	    def line = "N/A"
+	    def affected_code = issue["affectedCode"]
+	    def hash = sh(returnStdout: true, script: "sha256sum \$(pwd)/${component} | awk 'NR==1{print \$1}'")    
+            hash = hash.replace("\n", " ")
+	    if (title.matches("[a-zA-Z0-9].*")){
+		vulns.add([title, message, component, line, affected_code, hash, sev, "Nuclei"])
+	    }
+        }
 
         notifier.sendMessage('','good','Stage: "SAST-SonarResults": SUCCESS')
     }
