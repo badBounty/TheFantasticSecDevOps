10c84a4315eda8cbb69bbde90ea443b9ca857cd4
Kalust <39742600+Kalust@users.noreply.github.com>
2022-01-06 12:45:42 -0300

    Update DependenciesReplaceNodeJS.groovy

--- Jenkins/PipelineScripts/DependenciesReplaceNodeJS.groovy
+++ Jenkins/PipelineScripts/DependenciesReplaceNodeJS.groovy
@@ -2,6 +2,8 @@ def runStage(notifier)
 {
     try
     {
+        //Only use if package.json from remote repository has bitbucket package dependency.
+        
         notifier.sendMessage('','good','Stage: "Dependencies_Replace": INIT')
 
         withCredentials([usernamePassword(credentialsId: 'git-code-token-nodeJS', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')])
