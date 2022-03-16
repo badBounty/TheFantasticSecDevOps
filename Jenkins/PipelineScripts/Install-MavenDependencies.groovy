bcf8d43ec2c36f5c502d268e56593d234174ad51
Kalust <39742600+Kalust@users.noreply.github.com>
2022-03-16 10:46:10 -0300

    Update Install-MavenDependencies.groovy

--- Jenkins/PipelineScripts/Install-MavenDependencies.groovy
+++ Jenkins/PipelineScripts/Install-MavenDependencies.groovy
@@ -1,21 +1,17 @@
 def runStage(notifier)
 {
 	notifier.sendMessage('','good','Stage: "Install-Dependencies": INIT')
-
 	try
 	{
 		sh 'mvn clean install -X -DskipTests'
-	    	
 		notifier.sendMessage('','good','Stage: "Install-Dependencies": SUCESS')
-
-	} catch(Exception e)
+	} 
+	catch(Exception e)
 	{
 		notifier.sendMessage('','danger','Stage: "Install-Dependencies": FAILURE')
-		
 		currentBuild.result = 'FAILURE'
 		print('Stage: "Install-Dependencies": FAILURE')
 		print(e.printStackTrace())
-
 	}
 }
 return this
