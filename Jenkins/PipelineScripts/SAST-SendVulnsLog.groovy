4d58ab56b028c84964512aeede189e2c56fdbea6
Kalust <39742600+Kalust@users.noreply.github.com>
2022-03-23 15:05:13 -0300

    Update SAST-SendVulnsLog.groovy

--- Jenkins/PipelineScripts/SAST-SendVulnsLog.groovy
+++ Jenkins/PipelineScripts/SAST-SendVulnsLog.groovy
@@ -1,24 +1,43 @@
-def runStage(notifier)
+def runStage(notifier, vulns)
 {
     try 
     {
         notifier.sendMessage('','good','Stage: "SAST-SendVulnsLog": INIT')
+	    
         def projname = env.JOB_NAME
-        sshagent(['ssh-key-SAST-server']) 
-        {
-            def logs = sh(script: "ssh -o StrictHostKeyChecking=no ${env.SAST_Server_User}@${env.SAST_Server_IP} cat titleNormalization-${projname}.log", returnStdout: true).trim()
-            sh "ssh -o StrictHostKeyChecking=no ${env.SAST_Server_User}@${env.SAST_Server_IP} rm titleNormalization-${projname}.log"
-            notifier.sendMessage('','good',"Stage: SAST-SendVulnsLog: ${logs}")
-        }
+        def git_branch = env.branch
+	    
+	vulns.each
+        { vuln ->
+            def title = vuln[0]
+            def line = vuln[3]
+            def affected_code = vuln[4]
+            String listAffectedCode = String.join(", ", affected_code);
+            def affectedCodeParsed = listAffectedCode.tr(/"/,/'/);
+            def severity = vuln[6]
+            def origin = vuln[7]
+		
+	    def data = """{
+                "Project": "${projname}",
+		"Branch": "${git_branch}",
+		"Vuln title": "${title}",
+		"Severity_tool": "${severity.toLowerCase()}",
+		"Affected_code": "${listAffectedCode}",
+                "Line": "${line}",
+                "Origin": "${origin}"
+            }"""
+            
+            notifier.sendMessage('','#fab73c',"-----------------------------------------------------------------------------------------------")
+	    notifier.sendMessage('','#fab73c',"${data}")
+	}
 
         notifier.sendMessage('','good','Stage: "SAST-SendVulnsLog": SUCCESS')
     }
     catch(Exception e) 
     {
         notifier.sendMessage('','danger','Stage: "SAST-SendVulnsLog": FAILURE')	
-        
-		currentBuild.result = 'FAILURE'
-		print('Stage: "SAST-Deployment": FAILURE')
+	currentBuild.result = 'FAILURE'
+	print('Stage: "SAST-SendVulnsLog": FAILURE')
         print(e.printStackTrace())
     }
 }
