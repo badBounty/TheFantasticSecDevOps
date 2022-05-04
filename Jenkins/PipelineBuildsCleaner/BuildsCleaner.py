6257de63844c3cbff31d59655d4431a28c2c7e9f
Kalust <39742600+Kalust@users.noreply.github.com>
2022-05-04 17:23:02 -0300

    Update BuildsCleaner.py

--- Jenkins/PipelineBuildsCleaner/BuildsCleaner.py
+++ Jenkins/PipelineBuildsCleaner/BuildsCleaner.py
@@ -10,29 +10,35 @@ projectsDict = dict()
 def startCleaner():
     try:
         os.chdir(definedPathProjects)
-        subfolders = [ f.path for f in os.scandir(definedPathProjects) if f.is_dir() ]
+        subfolders = [ f.name for f in os.scandir(definedPathProjects) if f.is_dir() ]
         for folder in subfolders:
             if not folder.__contains__("tmp"):
                 #projectsDict[folder] = []
                 global project 
                 project = folder
+                definedPathBuilds = f"/var/jenkins_home/jobs/{project}/builds/"
                 os.chdir(definedPathBuilds)
-                subfolderBuilds = [ f.path for f in os.scandir(definedPathBuilds) if f.is_dir() and f.name.isnumeric() ]
-                subfolderBuilds.remove(max(subfolderBuilds))
+                subfolderBuilds = [ f.name for f in os.scandir(definedPathBuilds) if f.is_dir() and f.name.isnumeric() ]
+                subfolderBuilds.remove(max(subfolderBuilds, key=int))
                 projectsDict[folder] = subfolderBuilds
-        deleteBuilds(projectsDict)
+        print(projectsDict)
+        deleteBuilds(projectsDict, definedPathBuilds)
     except:
         printError(sys.exc_info())
     pass
 
-def deleteBuilds(projectsDict):
+def deleteBuilds(projectsDict, definedPathBuilds):
     try:
         for directoryProject in projectsDict:
             global project
             project = directoryProject
             os.chdir(definedPathBuilds)
             for build in projectsDict[directoryProject]:
-                shutil.rmtree(f"{definedPathBuilds}/{build}/", ignore_errors=True)
+                try:
+                    if not projectsDict[directoryProject]:
+                        shutil.rmtree(f"{definedPathBuilds}/{build}/", ignore_errors=True)
+                except:
+                    printError(sys.exc_info())
     except:
         printError(sys.exc_info())
     pass
