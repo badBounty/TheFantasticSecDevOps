04b0d896f9aadc7f5ad2bdc64c5b93f9c1716fb5
Kalust <39742600+Kalust@users.noreply.github.com>
2022-05-31 15:41:21 -0300

    Update parseDependencyCheckResults.py

--- SAST/parseDependencyCheckResults.py
+++ SAST/parseDependencyCheckResults.py
@@ -1,4 +1,3 @@
-import re
 import json
 import sys
 
@@ -11,31 +10,14 @@ def parserDC(fname, to):
                 l = line.split(',')
                 try:
                     dependency = l[3]
-                    Severity = l[18].upper()
-                    global maxSeverity
-                    if (Severity in severityDict):
-                        if (severityDict[Severity] > severityDict[maxSeverity]):
-                            maxSeverity = Severity
                     if (not(dependency in vulns)):
                         vulns.append(dependency)
                 except:
-                    print('Error')
+                    print("Error:\n")
+                    print(sys.exc_info())
     except:
-        print('Error')     
+        print("Error:\n")
+        print(sys.exc_info())    
     json.dump(vulns, open(to, 'w'), indent=2, sort_keys=True)
 
-if __name__ == "__main__":
-    if (len(sys.argv) != 4):
-        print('Parameters error')
-        sys.exit()
-    severityDict = {
-        "NULL": -1,
-        "LOW":0,
-        "MEDIUM":1,
-        "HIGH": 2,
-        "CRITICAL":3
-    }
-    maxSeverity = "NULL"
-    parserDC(sys.argv[1], sys.argv[2])
-    with open(sys.argv[3], 'w') as f:
-        f.write(maxSeverity.capitalize())
+parserDC(sys.argv[1], sys.argv[2])
