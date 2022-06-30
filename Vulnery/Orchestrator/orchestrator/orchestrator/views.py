71f4c38fb0e0fe7f073bd84c95db1e9098e9a8a9
Kalust <39742600+Kalust@users.noreply.github.com>
2022-06-30 18:43:19 -0300

    Delete Vulnery directory

--- Vulnery/Orchestrator/orchestrator/orchestrator/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
-
-#View default page.
-
-def defaultPage(request):
-    return render(request, "defaultPage.html")
