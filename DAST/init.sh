819c05b42780c18c422c0e117af29968aa57ada7
Maxpowersi <maxi1990@gmail.com>
2022-08-23 15:23:02 -0300

    Delete init.sh

--- DAST/init.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/usr/sbin/sshd -D &
-(export ZAP_AUTH_HEADER_VALUE=$1; echo $ZAP_AUTH_HEADER_VALUE; ./zap.sh -daemon -host 0.0.0.0 -port 8080 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true -config api.key=fvm39bpj135u20812je6ibgupv)
