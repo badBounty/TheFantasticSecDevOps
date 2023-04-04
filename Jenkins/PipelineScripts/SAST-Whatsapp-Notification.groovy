8bec08d60b85c19105c7a51f031c97817b3f4a04
Niquill0 <85759792+nicovarvara@users.noreply.github.com>
2023-04-04 14:01:02 -0300

    Update SAST-Whatsapp-Notification.groovy

--- Jenkins/PipelineScripts/SAST-Whatsapp-Notification.groovy
+++ Jenkins/PipelineScripts/SAST-Whatsapp-Notification.groovy
@@ -6,8 +6,12 @@ stage('SAST-Whatsapp-Notification')
   steps{
       script
       {
+        try {
           sh 'curl -I "https://api.callmebot.com/whatsapp.php?phone=+5491132617901&text=Termino+la+ejecucion+de+{repoName}&apikey=439147"
-              
+        }
+        catch(Exception e){
+              print(e.getMessage())
+          }
       }
     }
 }
