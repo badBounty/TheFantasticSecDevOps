3e615a85e57229e01445c7f35a2243f291b988a8
niquillo <nicovarvara>
2023-04-20 16:46:30 -0400

    modificacion en cloning

--- Jenkins/PipelineScripts/SAST-Cloning.groovy
+++ Jenkins/PipelineScripts/SAST-Cloning.groovy
@@ -3,7 +3,7 @@ def runStage() {
         sshagent(['ssh-key-SAST-image']) {
             sh "ssh-keygen -f '/var/jenkins_home/.ssh/known_hosts' -R [${env.SAST_SERVER_IP}]:${env.SAST_SERVER_SSH_PORT}"
     	    sh "ssh -p ${env.SAST_SERVER_SSH_PORT} -o StrictHostKeyChecking=no root@${env.SAST_SERVER_IP} rm -rf /home/${env.REPO_TO_SCAN_NAME}"
-	        sh "scp -P ${env.SAST_SERVER_SSH_PORT} -o StrictHostKeyChecking=no -v -r \$(pwd) root@${env.SAST_SERVER_IP}:/home"
+	        sh "scp -P ${env.SAST_SERVER_SSH_PORT} -o StrictHostKeyChecking=no -v -r \$(pwd)/${env.REPO_TO_SCAN_NAME} root@${env.SAST_SERVER_IP}:/home"
         }
         print('Stage: "SAST-Cloning": SUCCESS')	
     }
