14a1fde27fd342f710429827edd0354e7d449e29
Niquill0 <85759792+nicovarvara@users.noreply.github.com>
2023-04-24 14:53:31 -0300

    Update SAST-Cloning.groovy

--- Jenkins/PipelineScripts/SAST-Cloning.groovy
+++ Jenkins/PipelineScripts/SAST-Cloning.groovy
@@ -4,6 +4,7 @@ def runStage() {
             sh "ssh-keygen -f '/var/jenkins_home/.ssh/known_hosts' -R [${env.SAST_SERVER_IP}]:${env.SAST_SERVER_SSH_PORT}"
     	    sh "ssh -p ${env.SAST_SERVER_SSH_PORT} -o StrictHostKeyChecking=no root@${env.SAST_SERVER_IP} rm -rf /home/${env.REPO_TO_SCAN_NAME}"
 		sh "pwd"
+		sh "ls"
 	    sh "scp -P ${env.SAST_SERVER_SSH_PORT} -o StrictHostKeyChecking=no -v -r \$(pwd) root@${env.SAST_SERVER_IP}:/home"
 		sh "ssh -p ${env.SAST_SERVER_SSH_PORT} -o StrictHostKeyChecking=no root@${env.SAST_SERVER_IP} ls"
         }
